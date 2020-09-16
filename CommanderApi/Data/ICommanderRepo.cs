6eda2d93a776c25fde93653c781dbb42c9bc6ce3
HBSISGustavoHamerschmidt <gustavo.h@hbsis.com.br>
2020-09-15 22:37:20 -0300

    Adding Mock Repository (data)

---
+++ CommanderApi/Data/ICommanderRepo.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Commander.Models;
+
+namespace Commander.Data
+{
+    public interface ICommanderRepo
+    {
+        // Give me a list of all our command objects
+        IEnumerable<Command> GetAllCommands();
+
+        // Return me a single command based on an Id
+        Command GetCommandById(int id);
+
+
+    }
+}
---
+++ CommanderApi/Models/Command.cs
@@ -0,0 +1,17 @@
+﻿namespace Commander.Models
+{
+    public class Command
+    {
+        // Id of the Command
+        public int Id { get; set; }
+
+        // How to use the command
+        public string HowTo { get; set; }
+        
+        // Command Line
+        public string Line { get; set; }
+
+        // Platform the command is to be used
+        public string Platform { get; set; }
+    }
+}
